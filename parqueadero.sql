6025e7ef19d0a795ae0acaa22698b1477de7a726
JHONSEBASTIANRODRIGUEZBOHADA <104032255+JHONSEBASTIANRODRIGUEZBOHADA@users.noreply.github.com>
2023-06-01 15:50:56 +0100

    Add files via upload

---
+++ parqueadero.sql
@@ -0,0 +1,26 @@
+create database parqueadero;
+use parqueadero;
+
+CREATE TABLE `parqueadero`.`conductor` (
+  `conId` INT NOT NULL,
+  `conTipoDocumento` VARCHAR(45) NOT NULL,
+  `conDocumento` VARCHAR(10) NOT NULL,
+  `conNombre` VARCHAR(45) NOT NULL,
+  `conApellido` VARCHAR(45) NOT NULL,
+  `conTelefono` VARCHAR(10) NOT NULL,
+  PRIMARY KEY (`conId`));
+  
+  CREATE TABLE `parqueadero`.`vehiculo` (
+  `vehId` INT NOT NULL,
+  `vehTipoVehiculo` VARCHAR(9) NOT NULL,
+  `vehPlacaVehiculo` VARCHAR(7) NOT NULL,
+  `vehDetallesVehiculo` VARCHAR(100) NOT NULL,
+  `vehEstado` VARCHAR(45) NOT NULL,
+  PRIMARY KEY (`vehId`));
+  
+   CREATE TABLE `parqueadero`.`tiempo` (
+  `tieId` INT NOT NULL,
+  `tieHoraEntrada` DATE NOT NULL,
+  `tieHoraSalida` DATE NOT NULL,
+  `tieValorTotal` DOUBLE NOT NULL,
+  PRIMARY KEY (`tieId`)); 
